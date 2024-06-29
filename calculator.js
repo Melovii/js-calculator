b2eaa503814c115167b7c30a95fab141092b4f5a
Melovii <145908254+Melovii@users.noreply.github.com>
2024-06-29 11:14:27 +0300

    fixed operator button override, incorrect input calculation

--- calculator.js
+++ calculator.js
@@ -4,50 +4,87 @@ let operand2 = 0;
 let op = '';
 let currentInput = '';
 
-const display = document.querySelector('.display');
+const currentOperation = document.querySelector('.currentOperation');
+const lastOperation = document.querySelector('.lastOperation');
 
-const allButtons = document.querySelectorAll('.btn');
-allButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // button.blur(); idk what to do here TODO: IDK MAN
-    });
+const equalButton = document.querySelector('.equals');
+equalButton.addEventListener('click', () => {
+    operand1 = parseFloat(lastOperation.textContent.trim().slice(0, -1));
+    operand2 = parseFloat(currentOperation.textContent.trim());
+    op = lastOperation.textContent.trim().slice(-1);
+
+    result = operate(operand1, operand2, op);
+    currentInput = result;
+    currentOperation.textContent = currentInput;
+    lastOperation.textContent += ` ${operand2} =`;
+    console.log(`operand 1: ${operand1}, operator: ${op}, operand 2: ${operand2}`);
 });
 
 const deleteButton = document.querySelector('.delete');
 deleteButton.addEventListener('click', () => {
-    let string = display.textContent;
-    let newString = string.slice(0, -1);
-    currentInput = newString;
-    display.textContent = currentInput;
+    currentInput = currentOperation.textContent.slice(0, -1);
+    currentOperation.textContent = currentInput;
 })
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
     currentInput = '';
-    display.textContent = currentInput;
+    currentOperation.textContent = currentInput;
+    lastOperation.textContent = currentInput;
 });
 
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         currentInput += button.textContent;
-        display.textContent = currentInput;
+        currentOperation.textContent = currentInput;
     });
 });
 
-operate(operand1, operand2, op);
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let emptyFlag = currentInput === '';
+        let operand = button.textContent;
+        let text = lastOperation.textContent.trim();
+
+        if (!emptyFlag) {
+            lastOperation.textContent = `${currentInput} ${operand} `;
+        } else {
+            if (text.endsWith('+') || text.endsWith('-') || text.endsWith('x') || text.endsWith('÷')) {
+                lastOperation.textContent = `${text.slice(0, -1)} ${operand}`;
+            } else {
+                lastOperation.textContent = `${text} ${operand}`;
+            }
+        }
+
+        let operatorr = lastOperation.textContent.trim().slice(-1);
+        console.log(`operator from the operator button: ${operatorr}`);
+        if (operatorr === '+' || operatorr === '-' || operatorr === 'x' || operatorr === '÷') {
+            currentInput = '';
+        }
+    });
+});
 
 function operate (n1, n2, op) {
-
     switch (op) {
-        case "-": n1 - n2;
+        case "-": return n1 - n2;
         break;
-        case "+": n1 + n2;
+        case "+": return n1 + n2;
         break;
-        case "x": n1 * n2;
+        case "x": return n1 * n2;
         break;
-        case "÷": n1 / n2;
+        case "÷":
+            if (n2 == 0) {
+                return "lmao, dividing by 0?";
+            } else {
+                return n1 / n2;
+            }
         break;
         default: alert('UGHHH SWITCH CASE ERROR');
     }
 }
+
+// function formatNumber(number, decimals = 2) {
+//     return number.toFixed(decimals);
+// }
